db22791d46f10a1fbfb64d69a1f1a8c3dbd6e478
Li Xiang <Li Xiang@DESKTOP-RK3EH9N>
2019-11-26 16:54:59 +0800

    "第一个demo"

--- sb_1909_demo1/src/main/java/com/hqyj/demo/Sb1909Demo1Application.java
+++ sb_1909_demo1/src/main/java/com/hqyj/demo/Sb1909Demo1Application.java
@@ -2,9 +2,17 @@ package com.hqyj.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Sb1909Demo1Application {
+public class Sb1909Demo1Application extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Sb1909Demo1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sb1909Demo1Application.class, args);
